e2298fb04c0305dbfe35a637b8f4fccd96c56887
Johnson <johnson81941@gmail.com>
2018-03-27 19:58:08 +0800

    sql first connection.

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connectionString = "Server=tcp:receiversqldatabase.database.windows.net,1433;Initial Catalog=Receiver_SQL;Persist Security Info=False;User ID=johnson0317;Password=Johnson19970317;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            string sql = "SELECT * from Table2";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlDataAdapter dataadapter = new SqlDataAdapter(sql, connection);
+            DataSet ds = new DataSet();
+            connection.Open();
+            dataadapter.Fill(ds, "Table2");
+            connection.Close();
+            dataGridView1.DataSource = ds;
+            dataGridView1.DataMember = "Table2";
+        }
+    }
+}
